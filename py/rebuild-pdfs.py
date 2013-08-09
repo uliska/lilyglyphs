b7832d6030f538cd431ea294db9a2a11076be392
uliska <git@ursliska.de>
2013-08-09 19:34:58 +0200

    Update License preamble in all files

--- py/rebuild-pdfs.py
+++ py/rebuild-pdfs.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python
 
-# ########################################################################
-#                                                                        #
-#      This file is part of the 'lilyglyphs' LaTeX package.              #
-#                                ==========                              #
-#                                                                        #
-#              https://github.com/uliska/lilyglyphs                      #
-#                                                                        #
-#  Copyright 2012 by Urs Liska, git@ursliska.de                          #
-#                                                                        #
-#  'lilyglyphs' is free software: you can redistribute it and/or modify  #
-#  it under the terms of the GNU General Public License as published by  #
-#  the Free Software Foundation, either version 3 of the License, or     #
-#  (at your option) any later version.                                   #
-#                                                                        #
-#  This program is distributed in the hope that it will be useful,       #
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
-#  GNU General Public License for more details.                          #
-#                                                                        #
-#  You should have received a copy of the GNU General Public License     #
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>. #
-#                                                                        #
-# ########################################################################
-
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#                                                                        %
+#      This file is part of the 'lilyglyphs' LaTeX package.              %
+#                                ==========                              %
+#                                                                        %
+#              https://github.com/openlilylib/lilyglyphs                 %
+#               http://www.openlilylib.org/lilyglyphs                    %
+#                                                                        %
+#  Copyright 2012-2013 Urs Liska and others, ul@openlilylib.org          %
+#                                                                        %
+#  'lilyglyphs' is free software: you can redistribute it and/or modify  %
+#  it under the terms of the LaTeX Project Public License, either        %
+#  version 1.3 of this license or (at your option) any later version.    %
+#  You may find the latest version of this license at                    %
+#               http://www.latex-project.org/lppl.txt                    %
+#  more information on                                                   %
+#               http://latex-project.org/lppl/                           %
+#  and version 1.3 or later is part of all distributions of LaTeX        %
+#  version 2005/12/01 or later.                                          %
+#                                                                        %
+#  This work has the LPPL maintenance status 'maintained'.               %
+#  The Current Maintainer of this work is Urs Liska (see above).         %
+#                                                                        %
+#  This work consists of the files listed in the file 'manifest.txt'     %
+#  which can be found in the 'license' directory.                        %
+#                                                                        %
+#  This program is distributed in the hope that it will be useful,       %
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        %
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  %
+#                                                                        %
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ 
 # ########################################################################
 #                                                                        #
 #   rebuild-pdfs.py                                                      #
