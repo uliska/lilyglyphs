c7efa35fbb3afaf3a38c51d406d6fecb6ca3d1a1
uliska <git@ursliska.de>
2013-09-03 18:14:45 +0200

    deploy.py: write new script for v0.2.1

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -44,74 +44,183 @@
 #                                                                        #
 # ########################################################################
 
-import lilyglyphs_common as lg, os, sys, tarfile, shutil
+import os, sys, shutil, zipfile, argparse, datetime
 
 # ################
 # Global variables
 
-dir_deploy = 'deploy/'
-tar = None
-
+dir_deploy = 'deploy/lilyglyphs'
+manifest = []
+    
 def main():
-    global tar,  version
-    lg.check_lilyglyphs_root()
-    if not os.path.exists(dir_deploy):
-        os.mkdir(dir_deploy)
-    archive_name = dir_deploy + 'lilyglyphs-' + version + '.tar.gz'
-    tar = tarfile.open(archive_name, 'w:gz')
+    
+    if os.path.exists('deploy'):
+        overwrite = raw_input('deploy directory exists. Overwrite? ')
+        if overwrite != 'y':
+            print 'Aborted'
+            sys.exit(1)
+        shutil.rmtree('deploy')
+    os.mkdir('deploy')
+    os.mkdir(dir_deploy)
+    
+    # now we're guaranteed to have an empty 'deploy/lilyglyphs' directory
+    
+    print 'Copying files:'
+    # copy individual files from the root directory
+    cp_root()
+    # copy complete directories to their corresponding dest
+    cp_folder('commands', 'tex/commands')
+    cp_folder('core', 'tex/core')
+    cp_folder('fonts')
+    cp_folder('license')
+    cp_folder('source')
+    
+    # copy lilyglyphs.sty to /tex
+    shutil.copy2('lilyglyphs.sty', dir_deploy + '/tex/lilyglyphs.sty')
+    manifest.append('/tex/lilyglyphs.sty')
+    
 
-    print 'Add files to ' + archive_name
-    add_root()
-    add_documentation()
-    add_glyphimages()
-    tar.close()
+    # copy directories to different locations
+    cp_folder('glyphimages/definitions', 'source/glyphimages/definitions')
+    cp_folder('glyphimages/generated_src', 'source/glyphimages/generated_src')
+    cp_folder('glyphimages/pdfs', 'tex/pdfs')
+    cp_scripts()
+    cp_doc()
+    lg_private()
     
-    tar = tarfile.open('deploy/lilyglyphs-fonts.tar.gz', 'w:gz')
-    tar.add('otf')
-    tar.close()
+    write_manifest()
+    write_readme()
     
-def add_documentation():
-    global tar,  version
-    f = []
-    tar.add('documentation/lilyglyphs.pdf', 'lilyglyphs-' + version + '.pdf')
-    shutil.copy2('documentation/lilyglyphs.pdf', 'deploy/lilyglyphs-' + version + '.pdf')
+    # finally pack everything in a zip.
+    
+    zip = zipfile.ZipFile('deploy/lilyglyphs.zip', 'w')
+    os.chdir('deploy')
+    for root, dirs, files in os.walk('lilyglyphs/'):
+        for file in files:
+            zip.write(os.path.join(root, file))
+    zip.close()
+
     
     
-def add_files(files,  dir):
-    global tar
-    for file in files:
-        if not os.path.exists(file):
-            print 'Missing from ' + dir + ': ' + file
-        else:
-            tar.add(file)
+
+def copy_files(f, dst):
+    global manifest
+    for file in f:
+        shutil.copy2(file, dir_deploy + '/' + dst)
+        manifest.append(dst + '/' + file)
     
-def add_glyphimages():
-    f = []
-    f.append('glyphimages/pdfs')
-    logo_base = 'glyphimages/lilyglyphs_logo/lilyglyphs_logo.'
-    f.append(logo_base + 'pdf')
-    f.append(logo_base + 'png')
-    add_files(f, 'glyphimages')
+def append_manifest(src, dest):
+    for file in os.listdir(src):
+        if os.path.isdir(src + '/' + file):
+            append_manifest(src + '/' + file, dest + '/' + file)
+        manifest.append('/' + dest + '/' + file)
+
+def cp_doc():
+    cp_folder('documentation/resources')
+    for file in os.listdir('documentation'):
+        base, ext = os.path.splitext(file)
+        if ext in ['.tex', '.pdf', '.png', '.html']:
+            shutil.copy2('documentation/' + file, dir_deploy + '/documentation/' + file)
+            manifest.append('/deploy/documentation/' + file)
+    os.mkdir(dir_deploy + '/documentation/lilyglyphs_logo')
+    for file in os.listdir('documentation/lilyglyphs_logo'):
+        base, ext = os.path.splitext(file)
+        if ext in ['.tex', '.pdf', '.png']:
+            shutil.copy2('documentation/lilyglyphs_logo/' + file, 
+                dir_deploy + '/documentation/lilyglyphs_logo/' + file)
+            manifest.append('/deploy/documentation/lilyglyphs_logo/' + file)
     
-def add_root():
+    
+def cp_folder(src, dest = None):
+    global manifest
+    if not dest:
+        dest = src
+    print '-', src
+    shutil.copytree(src, dir_deploy + '/' + dest)
+    append_manifest(src, dest)
+    
+def cp_root():
+    print '- root'
     f = []
     f.append('CHANGES.md')
-    f.append('COPYING')
-    f.append('INSTALL.md')
-    f.append('README.md')
-    f.append('lilyglyphs.sty')
-    f.append('lilyglyphsStyle.sty')
-    f.append('commands')
-    f.append('core')
-    f.append('glyphlist')
-    add_files(f, 'root')
+    f.append('INSTALL')
+    f.append('README')
+    copy_files(f, '')
+
+def cp_scripts():
+    print '- scripts'
+    os.mkdir(dir_deploy + '/bin')
+    os.mkdir(dir_deploy + '/lib')
+    for file in os.listdir('scripts'):
+        if file.startswith('lily-'):
+            shutil.copy2('scripts/' + file, dir_deploy + '/bin')
+            manifest.append('/bin/' + file)
+        if file.startswith('lilyglyphs') and file.endswith('.py'):
+            shutil.copy2('scripts/' + file, dir_deploy + '/lib')
+            manifest.append('/lib/' + file)
     
+def lg_private():
+    print '- lilyglyphs_private'
+    zip = zipfile.ZipFile(dir_deploy + '/documentation/lilyglyphs_private.zip', 'w')
+    for root, dirs, files in os.walk('lilyglyphs_private'):
+        for file in files:
+            zip.write(os.path.join(root, file))
+    zip.close()
     
+def write_readme():
+    fout = open(dir_deploy + '/VERSION', 'w')
+    fout.write('This is lilyglyphs ' + version + '\n')
+    fout.write('Generated on ' + str(datetime.date.today()) + '\n')
+    fout.close()
+    
+    fout = open(dir_deploy + '/tex/README', 'w')
+    fout.write('This directory contains files to be found by LaTeX.\n\n')
+    fout.write('The core/ and commands/ subdirectories\n')
+    fout.write('_must_ remain beside the lilyglyphs.sty package!\n\n')
+    fout.write('The pdfs/ subdirectory contains files\n')
+    fout.write('that are _used_ by the package.')
+    fout.close()
+    
+def write_manifest():
+    global manifest
+    fout = open(dir_deploy + '/license/MANIFEST', 'w')
+    
+    fout.write('MANIFEST for lilyglpyhs ' + version + '\n')
+    fout.write('Generated on ' + str(datetime.date.today()) + '\n\n')
+    fout.write('The following files are part of this work:\n\n')
+    manifest.sort()
+    for line in manifest:
+        fout.write(line + '\n')
+    fout.close()
+
+
 # ####################################
 # Finally launch the program
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print 'No version number argument given.\n'
-        sys.exit(2)
-    version = str(sys.argv[1])
+    
+    # parse command line arguments
+    parser = argparse.ArgumentParser(
+                      description='Prepare lilyglyphs for deployment')
+    parser.add_argument('v', 
+                        metavar='VERSION', 
+                        help='Version to be deployed.')
+    args = parser.parse_args()
+    version = vars(args)['v']
+    version_parts = version.split('.')
+    try:
+        for part in version_parts:
+            int(part)
+    except:
+        print 'Malformed version argument:', version
+        print 'Use three integers separated by dots'
+        sys.exit(1)
+    
+    print 'Preparing lilyglyphs deployment', version + '.'
+    print 'CWD is', os.getcwd()
+    print 'This should be the root of lilyglyphs.'
+    print 'This script will not work on Windows.'
+    check = raw_input('Proceed (y/..)? ')
+    if check != 'y':
+        sys.exit(1)
+    
     main()
